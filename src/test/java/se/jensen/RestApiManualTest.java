73ae564e7c1c6b3e13a44e3ac641f808a2ab9833
sophiazhang0111 <49613531+sophiazhang0111@users.noreply.github.com>
2020-11-11 14:46:00 +0100

    second one

--- src/test/java/se/jensen/RestApiManualTest.java
+++ src/test/java/se/jensen/RestApiManualTest.java
@@ -12,8 +12,6 @@ import java.util.stream.Collectors;
 public class RestApiManualTest {
     public static void main(String[] args) {
 
-        System.out.println("Hello world.");
-
         printAllEmployees();
         List<EmployeeModel> employeeModelsById = getAllEmployees().stream()
                 .map(employeeModel -> {
