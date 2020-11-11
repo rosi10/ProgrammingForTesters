6ea0bebc11f1340d3e5adf9027598f83c83dfb00
sophiazhang0111 <49613531+sophiazhang0111@users.noreply.github.com>
2020-11-11 14:39:29 +0100

    added a change

--- src/test/java/se/jensen/ReadWriteFile.java
+++ src/test/java/se/jensen/ReadWriteFile.java
@@ -2,6 +2,9 @@ package se.jensen;
 
 public class ReadWriteFile {
     public static void main(String[] args) {
+        System.out.println("Change has been found.");
+
+
         while (true) ;
     }
 }
