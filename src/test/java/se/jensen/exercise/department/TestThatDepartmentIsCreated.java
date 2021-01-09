de369e7455e9688ce680cdda059938966fd1d011
Subhashini <sarathy.na@gmail.com>
2021-01-09 23:44:08 +0100

    Corrected

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -18,13 +18,14 @@ public class TestThatDepartmentIsCreated {
         Integer departmentId = 1;
         String departmentName = "Accounts";
 
-            Department department = Department.builder()
-                    .departmentId(departmentId)
-                    .departmentName(departmentName)
-                    .build();
-            Assert.assertEquals(departmentId, department.getDepartmentId());
-            Assert.assertEquals(departmentName, department.getDepartmentName());
-        }
+        Department department = Department.builder()
+                .departmentId(departmentId)
+                .departmentName(departmentName)
+                .build();
+        Assert.assertEquals(departmentId, department.getDepartmentId());
+        Assert.assertEquals(departmentName, department.getDepartmentName());
+    }
+
     @Test(expected = NullPointerException.class)
     public void testThatNullValueAllowedForDepartmentID() {
         Department.builder()
@@ -35,46 +36,13 @@ public class TestThatDepartmentIsCreated {
     }
 
     @Test
-    public void testThatToStringReturnsProperly(){
+    public void testThatToStringReturnsProperly() {
         String expectedToString = DepartmentTestBuilder.build().toString();
         Department department = DepartmentTestBuilder.build();
-        Assert.assertEquals(expectedToString,department.toString());
+        Assert.assertEquals(expectedToString, department.toString());
     }
 
-
-
-
-    @Test
-    @DisplayName("Checks if the department is created")
-    public void CheckIfTheDepartmentIsCreated() {
-        String departmentName = "Intellij";
-        Integer departmentId = 2020;
-
-        Department department = Department.builder()
-                .departmentName(departmentName)
-                .departmentId(departmentId)
-                .build();
-
-        Assert.assertEquals(departmentName, department.getDepartmentName());
-        Assert.assertEquals(departmentId, department.getDepartmentId());
-    }
-
-    @Test(expected = NullPointerException.class)
-    @DisplayName("Checks if the departments name is Null")
-    public void CheckIfTheDepartmentNameIsNull(){
-        Department.builder()
-                .departmentName(null)
-                .departmentId(1)
-                .build();
-    }
-
-    @Test(expected = NullPointerException.class)
-    @DisplayName("Checks if the departments Id is Null")
-    public void CheckIfTheDepartmentIdIsNull(){
-        Department.builder()
-                .departmentName("Intellij")
-                .departmentId(null)
-                .build();
-    }
 }
+
+
 
